da2be14005233299e67bffdaafbc8d164af023fe
shashank7200 <shashank7200@gmail.com>
2017-03-05 00:00:57 +0530

    Menu - initial commit

---
+++ Menu/app/src/main/java/com/github/shashank7200/menu/MainActivity.java
@@ -0,0 +1,25 @@
+package com.github.shashank7200.menu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void printToLogs(View view) {
+        // Find first menu item TextView and print the text to the logs
+        TextView firstText =  (TextView) findViewById()
+        // Find second menu item TextView and print the text to the logs
+
+        // Find third menu item TextView and print the text to the logs
+
+    }
+}
