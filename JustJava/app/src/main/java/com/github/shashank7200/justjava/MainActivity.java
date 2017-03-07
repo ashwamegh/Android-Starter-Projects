18ddeb6379c842630e2dc5b6f36aa3d2b9f72985
shashank7200 <shashank7200@gmail.com>
2017-03-08 00:57:24 +0530

    Added Intent to mail the generated order

--- JustJava/app/src/main/java/com/github/shashank7200/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/github/shashank7200/justjava/MainActivity.java
@@ -1,5 +1,8 @@
 package com.github.shashank7200.justjava;
 
+import android.content.Intent;
+import android.location.Location;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +10,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void increment(View view){
+
+        if (numberOfCoffees == 100){
+            Toast.makeText(this, R.string.increment_toast_message,Toast.LENGTH_SHORT).show();
+            return;
+        }
         numberOfCoffees++;
         displayQuantity(numberOfCoffees);
     }
@@ -34,12 +43,18 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void decrement(View view){
+
+        if (numberOfCoffees == 1){
+            Toast.makeText(this, R.string.decrement_toast_message,Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         numberOfCoffees--;
         displayQuantity(numberOfCoffees);
     }
 
     private int calculatePrice(boolean addWhippedCream, boolean addChocolate){
-        int basePrice = numberOfCoffees * 5;
+        int basePrice = 5;
 
         if (addWhippedCream){
             if (addChocolate){
@@ -55,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        return  basePrice;
+        return  basePrice * numberOfCoffees ;
     }
     /**
      * This method is called when ORDER button is pressed
@@ -75,7 +90,18 @@ public class MainActivity extends AppCompatActivity {
 
 
         String orderSummary = createOrderSummary(nameString, orderPrice ,hasWhippedCream, hasChocolate);
-        displayMessage(orderSummary);
+
+        /**
+         * Intent which invokes an email app and passes Email Subject & messagae to populate intent data.
+         */
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_subject)+" "+nameString);
+        intent.putExtra(Intent.EXTRA_TEXT, orderSummary);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
 
     }
 
@@ -89,17 +115,16 @@ public class MainActivity extends AppCompatActivity {
     private String createOrderSummary(String name, int orderPrice, boolean hasWhippedCream , boolean hasChocolate) {
 
 
-        String orderSummary  = "Name: "+name+"\nAdd whipped cream? "+hasWhippedCream+
-                "\nAdd chocolate? "+hasChocolate+
-                "\nQuantity: "+numberOfCoffees+
-                "\nTotal: ₹ "+orderPrice+"\nThank You!";
+        String orderSummary  = getString(R.string.order_summary_name,name)+"\n"+
+                getString(R.string.whipped_cream_status,hasWhippedCream)+"\n"+
+                getString(R.string.chocolate_status, hasChocolate)+"\n"+
+                getString(R.string.order_quantity, numberOfCoffees)+"\n"+
+                getString(R.string.total_price, orderPrice)+"\n"+
+                getString(R.string.thank_you);
+
         return orderSummary;
     }
 
-    private void displayMessage(String priceMessage) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(priceMessage);
-    }
 
     /**
      * This method updates the Quantity of Coffees being Ordered.
