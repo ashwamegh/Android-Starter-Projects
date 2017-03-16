e118e7b70c31bf80a13c1383119b8c99d6971e21
shashank7200 <shashank7200@gmail.com>
2017-03-17 00:40:03 +0530

    Modified layout, little improvements in logic

--- Quizster/app/src/main/java/com/github/shashank7200/quizster/SubmitActivity.java
+++ Quizster/app/src/main/java/com/github/shashank7200/quizster/SubmitActivity.java
@@ -1,7 +1,9 @@
 package com.github.shashank7200.quizster;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by AshwaMegh on 15-03-2017.
@@ -12,6 +14,11 @@ public class SubmitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
-    }
+        Intent i = getIntent();
 
+        int finalScore = i.getIntExtra("Score",0);
+
+        TextView scoreDisplay = (TextView) findViewById(R.id.score_display);
+        scoreDisplay.setText("Score: "+finalScore);
+    }
 }
